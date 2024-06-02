3f48e2ae798717be7771367fc8de4cb3b2725385
wiesty <basti@hypermail.de>
2024-06-03 01:02:30 +0200

    Projektdateien hinzufügen.

---
+++ ILM/main.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ILM
+{
+    partial class main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.buttonPlay = new MetroFramework.Controls.MetroButton();
+            this.comboBoxStations = new MetroFramework.Controls.MetroComboBox();
+            this.wmpPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(23, 150);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(182, 23);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play/Stop";
+            this.buttonPlay.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.buttonPlay.UseSelectable = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click_1);
+            // 
+            // comboBoxStations
+            // 
+            this.comboBoxStations.FormattingEnabled = true;
+            this.comboBoxStations.ItemHeight = 23;
+            this.comboBoxStations.Location = new System.Drawing.Point(23, 63);
+            this.comboBoxStations.Name = "comboBoxStations";
+            this.comboBoxStations.Size = new System.Drawing.Size(182, 29);
+            this.comboBoxStations.TabIndex = 2;
+            this.comboBoxStations.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.comboBoxStations.UseSelectable = true;
+            this.comboBoxStations.SelectedIndexChanged += new System.EventHandler(this.comboBoxStations_SelectedIndexChanged);
+            // 
+            // wmpPlayer
+            // 
+            this.wmpPlayer.Enabled = true;
+            this.wmpPlayer.Location = new System.Drawing.Point(354, 195);
+            this.wmpPlayer.Name = "wmpPlayer";
+            this.wmpPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpPlayer.OcxState")));
+            this.wmpPlayer.Size = new System.Drawing.Size(10, 10);
+            this.wmpPlayer.TabIndex = 3;
+            // 
+            // metroTrackBar1
+            // 
+            this.metroTrackBar1.BackColor = System.Drawing.Color.Transparent;
+            this.metroTrackBar1.Location = new System.Drawing.Point(23, 109);
+            this.metroTrackBar1.Name = "metroTrackBar1";
+            this.metroTrackBar1.Size = new System.Drawing.Size(182, 23);
+            this.metroTrackBar1.TabIndex = 6;
+            this.metroTrackBar1.Text = "metroTrackBar1";
+            this.metroTrackBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.metroTrackBar1_Scroll);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 201);
+            this.Controls.Add(this.metroTrackBar1);
+            this.Controls.Add(this.wmpPlayer);
+            this.Controls.Add(this.comboBoxStations);
+            this.Controls.Add(this.buttonPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "main";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.White;
+            this.Text = "ILM";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wmpPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton buttonPlay;
+        private MetroFramework.Controls.MetroComboBox comboBoxStations;
+        private AxWMPLib.AxWindowsMediaPlayer wmpPlayer;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
+    }
+}
+
---
+++ ILM/main.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using AxWMPLib;
+using MetroFramework.Forms;
+
+namespace ILM
+{
+    public partial class main : MetroForm
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadRadioStationsFromWebsite();
+            comboBoxStations.DisplayMember = "Key";
+            comboBoxStations.ValueMember = "Value";
+
+            this.metroTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.metroTrackBar1_Scroll);
+            wmpPlayer.PlayStateChange += WmpPlayer_PlayStateChange;
+        }
+
+        private void LoadRadioStationsFromWebsite()
+        {
+            string url = "https://ilovemusic.de/streams/";
+            WebClient client = new WebClient();
+            try
+            {
+                string htmlCode = client.DownloadString(url);
+                Console.WriteLine(htmlCode);
+                Dictionary<string, string> radioStations = ExtractRadioStations(htmlCode);
+                Console.WriteLine(radioStations);
+                foreach (var station in radioStations)
+                {
+                    comboBoxStations.Items.Add(station);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Herunterladen der Radiosender: " + ex.Message);
+            }
+        }
+
+        private Dictionary<string, string> ExtractRadioStations(string htmlCode)
+        {
+            Dictionary<string, string> radioStations = new Dictionary<string, string>();
+
+            string pattern = @"<a\s+href=""([^""]+\.m3u)""[^>]*>([^<]+)</a>";
+
+            MatchCollection matches = Regex.Matches(htmlCode, pattern);
+
+            foreach (Match match in matches)
+            {
+                string name = System.IO.Path.GetFileNameWithoutExtension(match.Groups[1].Value);
+                name = name.Replace("ilove", "I ♥ ");
+                string url = match.Groups[1].Value.Trim();
+                radioStations[name] = url;
+            }
+
+            return radioStations;
+        }
+
+
+
+        private void buttonPlay_Click_1(object sender, EventArgs e)
+        {
+            if (buttonPlay.Text == "switch channel")
+            {
+                var selectedStation = (KeyValuePair<string, string>)comboBoxStations.SelectedItem;
+                wmpPlayer.URL = selectedStation.Value;
+                wmpPlayer.Ctlcontrols.play();
+            }
+
+            if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying) 
+            {
+                wmpPlayer.Ctlcontrols.stop();
+                buttonPlay.Text = "Play"; 
+            }
+            else 
+            {
+                if (comboBoxStations.SelectedItem != null)
+                {
+                    var selectedStation = (KeyValuePair<string, string>)comboBoxStations.SelectedItem;
+                    wmpPlayer.URL = selectedStation.Value; 
+                    wmpPlayer.Ctlcontrols.play(); 
+                    buttonPlay.Text = "Stop";
+                }
+                else
+                {
+                    MessageBox.Show("Bitte wählen Sie einen Radiosender aus.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void WmpPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            // Wenn der Player den Status "Wiedergabe" erreicht, ändere den Text des Buttons auf "Stop"
+            if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                buttonPlay.Text = "Stop";
+            }
+            // Wenn der Player aufhört zu spielen, ändere den Text des Buttons zurück auf "Play"
+            else if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                buttonPlay.Text = "Play";
+            }
+        }
+
+
+        private void metroTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            wmpPlayer.settings.volume = metroTrackBar1.Value;
+        }
+
+        private void comboBoxStations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                buttonPlay.Text = "switch channel";
+            }
+            else
+            {
+                if (comboBoxStations.SelectedItem != null)
+                {
+                    buttonPlay.Text = "Play";
+                }
+                else
+                {
+                    MessageBox.Show("Bitte wählen Sie einen Radiosender aus.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
