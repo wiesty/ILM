a27938e68b42ea22838a1208b855e85e9de80ac2
wiesty <basti@hypermail.de>
2024-07-29 20:18:05 +0200

    V 2.0.0 added Volumefix, cover image, song info, version checker

--- ILM/main.Designer.cs
+++ ILM/main.Designer.cs
@@ -33,17 +33,19 @@
             this.comboBoxStations = new MetroFramework.Controls.MetroComboBox();
             this.metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
             this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.VersionLabel = new MetroFramework.Controls.MetroLabel();
+            this.currentPlaying = new MetroFramework.Controls.MetroLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.wmpPlayer = new AxWMPLib.AxWindowsMediaPlayer();
-            this.version = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.wmpPlayer)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonPlay
             // 
-            this.buttonPlay.Location = new System.Drawing.Point(31, 215);
-            this.buttonPlay.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonPlay.Location = new System.Drawing.Point(23, 179);
             this.buttonPlay.Name = "buttonPlay";
-            this.buttonPlay.Size = new System.Drawing.Size(243, 28);
+            this.buttonPlay.Size = new System.Drawing.Size(242, 29);
             this.buttonPlay.TabIndex = 0;
             this.buttonPlay.Text = "Play/Stop";
             this.buttonPlay.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -53,11 +55,10 @@
             // comboBoxStations
             // 
             this.comboBoxStations.FormattingEnabled = true;
-            this.comboBoxStations.ItemHeight = 24;
-            this.comboBoxStations.Location = new System.Drawing.Point(31, 78);
-            this.comboBoxStations.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxStations.ItemHeight = 23;
+            this.comboBoxStations.Location = new System.Drawing.Point(23, 63);
             this.comboBoxStations.Name = "comboBoxStations";
-            this.comboBoxStations.Size = new System.Drawing.Size(241, 30);
+            this.comboBoxStations.Size = new System.Drawing.Size(242, 29);
             this.comboBoxStations.Style = MetroFramework.MetroColorStyle.Black;
             this.comboBoxStations.TabIndex = 2;
             this.comboBoxStations.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -67,10 +68,9 @@
             // metroTrackBar1
             // 
             this.metroTrackBar1.BackColor = System.Drawing.Color.Transparent;
-            this.metroTrackBar1.Location = new System.Drawing.Point(31, 134);
-            this.metroTrackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.metroTrackBar1.Location = new System.Drawing.Point(23, 137);
             this.metroTrackBar1.Name = "metroTrackBar1";
-            this.metroTrackBar1.Size = new System.Drawing.Size(243, 28);
+            this.metroTrackBar1.Size = new System.Drawing.Size(242, 23);
             this.metroTrackBar1.TabIndex = 6;
             this.metroTrackBar1.Text = "metroTrackBar1";
             this.metroTrackBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -79,56 +79,80 @@
             // metroCheckBox1
             // 
             this.metroCheckBox1.FontSize = MetroFramework.MetroCheckBoxSize.Medium;
-            this.metroCheckBox1.Location = new System.Drawing.Point(31, 169);
+            this.metroCheckBox1.Location = new System.Drawing.Point(288, 228);
+            this.metroCheckBox1.Margin = new System.Windows.Forms.Padding(2);
             this.metroCheckBox1.Name = "metroCheckBox1";
-            this.metroCheckBox1.Size = new System.Drawing.Size(183, 29);
+            this.metroCheckBox1.Size = new System.Drawing.Size(140, 24);
             this.metroCheckBox1.TabIndex = 7;
-            this.metroCheckBox1.Text = "Volumefix";
+            this.metroCheckBox1.Text = "Background Mode";
             this.metroCheckBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.metroCheckBox1.UseSelectable = true;
             this.metroCheckBox1.CheckedChanged += new System.EventHandler(this.metroCheckBox1_CheckedChanged);
             // 
+            // VersionLabel
+            // 
+            this.VersionLabel.Location = new System.Drawing.Point(23, 228);
+            this.VersionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(82, 24);
+            this.VersionLabel.TabIndex = 8;
+            this.VersionLabel.Text = "V. 2.0.0";
+            this.VersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.VersionLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // currentPlaying
+            // 
+            this.currentPlaying.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.currentPlaying.Location = new System.Drawing.Point(23, 108);
+            this.currentPlaying.Margin = new System.Windows.Forms.Padding(0);
+            this.currentPlaying.Name = "currentPlaying";
+            this.currentPlaying.Size = new System.Drawing.Size(242, 23);
+            this.currentPlaying.TabIndex = 9;
+            this.currentPlaying.Text = "Please select a channel";
+            this.currentPlaying.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.currentPlaying.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ILM.Properties.Resources.ilmlogo;
+            this.pictureBox1.Location = new System.Drawing.Point(283, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
             // wmpPlayer
             // 
             this.wmpPlayer.Enabled = true;
-            this.wmpPlayer.Location = new System.Drawing.Point(346, 20);
-            this.wmpPlayer.Margin = new System.Windows.Forms.Padding(4);
+            this.wmpPlayer.Location = new System.Drawing.Point(23, 400);
             this.wmpPlayer.Name = "wmpPlayer";
             this.wmpPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpPlayer.OcxState")));
             this.wmpPlayer.Size = new System.Drawing.Size(10, 10);
             this.wmpPlayer.TabIndex = 3;
             // 
-            // version
-            // 
-            this.version.Location = new System.Drawing.Point(31, 264);
-            this.version.Name = "version";
-            this.version.Size = new System.Drawing.Size(241, 20);
-            this.version.TabIndex = 8;
-            this.version.Text = "V. 2.0.0";
-            this.version.TextAlign = System.Drawing.ContentAlignment.BottomRight;
-            this.version.Theme = MetroFramework.MetroThemeStyle.Dark;
-            // 
             // main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 299);
-            this.Controls.Add(this.version);
+            this.ClientSize = new System.Drawing.Size(454, 268);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.currentPlaying);
+            this.Controls.Add(this.VersionLabel);
             this.Controls.Add(this.metroCheckBox1);
             this.Controls.Add(this.metroTrackBar1);
             this.Controls.Add(this.wmpPlayer);
             this.Controls.Add(this.comboBoxStations);
             this.Controls.Add(this.buttonPlay);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "main";
-            this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
             this.Resizable = false;
             this.Style = MetroFramework.MetroColorStyle.White;
             this.Text = "ILM";
             this.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.wmpPlayer)).EndInit();
             this.ResumeLayout(false);
 
@@ -141,7 +165,9 @@
         private AxWMPLib.AxWindowsMediaPlayer wmpPlayer;
         private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
         private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
-        private MetroFramework.Controls.MetroLabel version;
+        private MetroFramework.Controls.MetroLabel VersionLabel;
+        private MetroFramework.Controls.MetroLabel currentPlaying;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ILM/main.cs
+++ ILM/main.cs
@@ -1,23 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
-using AxWMPLib;
 using MetroFramework.Forms;
+using NAudio.CoreAudioApi;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
 
 namespace ILM
 {
     public partial class main : MetroForm
     {
+        private MMDeviceEnumerator deviceEnumerator;
+        private MMDevice defaultDevice;
+        private string apiUrl = "https://ilovemusic.de/typo3conf/ext/ep_channel/Scripts/playlist.php";
+        private string versionUrl = "https://raw.githubusercontent.com/wiesty/ILM/master/VERSION";
+        private string currentVersion = "2.0.0";
+        private Dictionary<string, string> segmentNameMapping;
+        private System.Timers.Timer songUpdateTimer;
+
         public main()
         {
             InitializeComponent();
+            deviceEnumerator = new MMDeviceEnumerator();
+            defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            segmentNameMapping = new Dictionary<string, string>();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
-            LoadRadioStationsFromWebsite();
+            await LoadRadioStationsFromApi();
             comboBoxStations.DisplayMember = "Key";
             comboBoxStations.ValueMember = "Value";
 
@@ -25,125 +40,311 @@ namespace ILM
             wmpPlayer.PlayStateChange += WmpPlayer_PlayStateChange;
 
             metroCheckBox1.CheckedChanged += new EventHandler(this.metroCheckBox1_CheckedChanged);
-        }
+            currentPlaying.Click += new EventHandler(this.CurrentPlaying_Click);
+            this.VersionLabel.Click += new EventHandler(this.VersionLabel_Click);
 
-        private void LoadRadioStationsFromWebsite()
+            songUpdateTimer = new System.Timers.Timer(45000);
+            songUpdateTimer.Elapsed += async (s, ev) => await UpdateCurrentSongInfo();
+            songUpdateTimer.Start();
+        }
+        
+        private async Task LoadRadioStationsFromApi()
         {
-            string url = "https://ilovemusic.de/streams/";
-            WebClient client = new WebClient();
             try
             {
-                string htmlCode = client.DownloadString(url);
-                Dictionary<string, string> radioStations = ExtractRadioStations(htmlCode);
-                foreach (var station in radioStations)
+                using (HttpClient client = new HttpClient())
                 {
-                    comboBoxStations.Items.Add(station);
+                    string jsonResponse = await client.GetStringAsync(apiUrl);
+                    var data = JObject.Parse(jsonResponse);
+
+                    var stations = new List<object>();
+                    foreach (var channel in data.Properties())
+                    {
+                        string segmentName = channel.Value["segmentname"].ToString();
+                        if (!string.IsNullOrWhiteSpace(segmentName))
+                        {
+                            string displayName = $"I ♥ {segmentName.Replace("ilove", "")}";
+                            string streamUrl = $"https://www.ilovemusic.de/{segmentName}.m3u";
+
+                            segmentNameMapping[displayName] = segmentName;
+                            stations.Add(new KeyValuePair<string, string>(displayName, streamUrl));
+
+                            Console.WriteLine($"Found channel: {displayName} with URL: {streamUrl}");
+                        }
+                    }
+
+                    InvokeOnUiThread(() =>
+                    {
+                        comboBoxStations.Items.AddRange(stations.ToArray());
+                    });
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Fehler beim Herunterladen der Radiosender: " + ex.Message);
+                MessageBox.Show("Error fetching radio channels: " + ex.Message);
             }
         }
 
-        private Dictionary<string, string> ExtractRadioStations(string htmlCode)
-        {
-            Dictionary<string, string> radioStations = new Dictionary<string, string>();
-            string pattern = @"<a\s+href=""([^""]+\.m3u)""[^>]*>([^<]+)</a>";
-            MatchCollection matches = Regex.Matches(htmlCode, pattern);
-
-            foreach (Match match in matches)
-            {
-                string name = System.IO.Path.GetFileNameWithoutExtension(match.Groups[1].Value);
-                name = name.Replace("ilove", "I ♥ ");
-                string url = match.Groups[1].Value.Trim();
-                radioStations[name] = url;
-            }
-
-            return radioStations;
-        }
-
-        private void buttonPlay_Click_1(object sender, EventArgs e)
+        private async void buttonPlay_Click_1(object sender, EventArgs e)
         {
             if (buttonPlay.Text == "switch channel")
             {
-                var selectedStation = (KeyValuePair<string, string>)comboBoxStations.SelectedItem;
-                wmpPlayer.URL = selectedStation.Value;
-                wmpPlayer.Ctlcontrols.play();
+                var selectedStation = GetSelectedStation();
+                if (selectedStation.HasValue)
+                {
+                    wmpPlayer.URL = selectedStation.Value.Value;
+                    wmpPlayer.Ctlcontrols.play();
+                    await UpdateCurrentSongInfo();
+                }
             }
 
             if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
             {
                 wmpPlayer.Ctlcontrols.stop();
-                buttonPlay.Text = "Play";
+                InvokeOnUiThread(() =>
+                {
+                    buttonPlay.Text = "Play";
+                    currentPlaying.Text = "Stopped";
+                    pictureBox1.Image = ILM.Properties.Resources.ilmlogo;
+                });
             }
             else
             {
-                if (comboBoxStations.SelectedItem != null)
+                var selectedStation = GetSelectedStation();
+                if (selectedStation.HasValue)
                 {
-                    var selectedStation = (KeyValuePair<string, string>)comboBoxStations.SelectedItem;
-                    wmpPlayer.URL = selectedStation.Value;
+                    wmpPlayer.URL = selectedStation.Value.Value;
                     wmpPlayer.Ctlcontrols.play();
-                    buttonPlay.Text = "Stop";
+                    InvokeOnUiThread(() => buttonPlay.Text = "Stop");
+                    await UpdateCurrentSongInfo();
                 }
                 else
                 {
-                    MessageBox.Show("Bitte wählen Sie einen Radiosender aus.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Please select a radio station.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
 
-        private void WmpPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        private async void WmpPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
         {
             if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
             {
-                buttonPlay.Text = "Stop";
+                InvokeOnUiThread(() => buttonPlay.Text = "Stop");
+                await UpdateCurrentSongInfo();
             }
             else if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsStopped)
             {
-                buttonPlay.Text = "Play";
+                InvokeOnUiThread(() =>
+                {
+                    buttonPlay.Text = "Play";
+                    currentPlaying.Text = "Stopped";
+                    pictureBox1.Image = ILM.Properties.Resources.ilmlogo;
+                });
             }
         }
 
         private void metroTrackBar1_Scroll(object sender, ScrollEventArgs e)
         {
-            if (metroCheckBox1.Checked)
-            {
-                if (metroTrackBar1.Value >= 1 && metroTrackBar1.Value <= 10)
-                {
-                    wmpPlayer.settings.volume = 1;
-                }
-                else
-                {
-                    wmpPlayer.settings.volume = (int)(metroTrackBar1.Value / 10);
-                }
-            }
-            else
-            {
-                wmpPlayer.settings.volume = metroTrackBar1.Value;
-            }
+            wmpPlayer.settings.volume = metroTrackBar1.Value;
         }
 
         private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
         {
-            metroTrackBar1_Scroll(this, null);
+            if (metroCheckBox1.Checked)
+            {
+                SetApplicationVolume("ILM", 0.2f);
+            }
+            else if (!metroCheckBox1.Checked)
+            {
+                SetApplicationVolume("ILM", 1f);
+            }
+            
         }
 
         private void comboBoxStations_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (wmpPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
             {
-                buttonPlay.Text = "switch channel";
+                InvokeOnUiThread(() => buttonPlay.Text = "switch channel");
             }
             else
             {
-                if (comboBoxStations.SelectedItem != null)
+                var selectedStation = GetSelectedStation();
+                if (selectedStation.HasValue)
                 {
-                    buttonPlay.Text = "Play";
+                    InvokeOnUiThread(() => buttonPlay.Text = "Play");
                 }
                 else
                 {
-                    MessageBox.Show("Bitte wählen Sie einen Radiosender aus.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Please select a radio station.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private KeyValuePair<string, string>? GetSelectedStation()
+        {
+            KeyValuePair<string, string>? selectedStation = null;
+            InvokeOnUiThread(() =>
+            {
+                selectedStation = (KeyValuePair<string, string>?)comboBoxStations.SelectedItem;
+            });
+            return selectedStation;
+        }
+
+        private async Task<string> GetCurrentlyPlayingSongAsync(string segmentName)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string jsonResponse = await client.GetStringAsync(apiUrl);
+                    var data = JObject.Parse(jsonResponse);
+                    var channel = data.Properties().FirstOrDefault(p => p.Value["segmentname"].ToString() == segmentName);
+                    if (channel != null)
+                    {
+                        var artist = channel.Value["artist"].ToString();
+                        var title = channel.Value["title"].ToString();
+                        var coverUrl = "https://ilovemusic.de" + channel.Value["cover"].ToString();
+                        UpdateCoverImage(coverUrl);
+                        Console.WriteLine($"Current song: {artist} - {title}");
+                        return $"{artist} - {title}";
+                    }
+                    return "Unknown Song";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching song data: " + ex.Message);
+                return "Unknown Song";
+            }
+        }
+
+        private async Task UpdateCurrentSongInfo()
+        {
+            var selectedStation = GetSelectedStation();
+            if (selectedStation.HasValue)
+            {
+                var songInfo = await GetCurrentlyPlayingSongAsync(segmentNameMapping[selectedStation.Value.Key]);
+                InvokeOnUiThread(() =>
+                {
+                    currentPlaying.Text = songInfo;
+                });
+            }
+            else
+            {
+                InvokeOnUiThread(() =>
+                {
+                    currentPlaying.Text = "Stopped";
+                    pictureBox1.Image = ILM.Properties.Resources.ilmlogo;
+                });
+            }
+        }
+
+        private async void UpdateCoverImage(string coverUrl)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync(coverUrl);
+                    response.EnsureSuccessStatusCode();
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        var image = Image.FromStream(stream);
+                        InvokeOnUiThread(() =>
+                        {
+                            pictureBox1.Image = image;
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                InvokeOnUiThread(() =>
+                {
+                    pictureBox1.Image = ILM.Properties.Resources.ilmlogo;
+                });
+                MessageBox.Show("Error fetching cover image: " + ex.Message);
+            }
+        }
+
+        private void SetApplicationVolume(string exeName, float volume)
+        {
+            var process = Process.GetProcessesByName(exeName).FirstOrDefault();
+            if (process != null)
+            {
+                var volumeManager = new AudioManager();
+                volumeManager.SetApplicationVolume(process.Id, volume * 100); 
+            }
+        }
+
+        private void InvokeOnUiThread(Action action)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void CurrentPlaying_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentPlaying.Text) && currentPlaying.Text != "Stopped")
+            {
+                Clipboard.SetText(currentPlaying.Text);
+                MessageBox.Show("Song info copied to clipboard.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private async void VersionLabel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string onlineVersion = await client.GetStringAsync(versionUrl);
+                    if (onlineVersion.Trim() != currentVersion)
+                    {
+                        var result = MessageBox.Show("A new version is available. Do you want to be redirected to github?", "New Version Available", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                        if (result == DialogResult.Yes)
+                        {
+                            Process.Start(new ProcessStartInfo
+                            {
+                                FileName = "https://github.com/wiesty/ILM/releases",
+                                UseShellExecute = true
+                            });
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("You are using the latest version.", "No New Version", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error checking for version: " + ex.Message);
+            }
+        }
+    }
+
+    public class AudioManager
+    {
+        public void SetApplicationVolume(int pid, float volume)
+        {
+            var deviceEnumerator = new MMDeviceEnumerator();
+            var defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            var sessions = defaultDevice.AudioSessionManager.Sessions;
+
+            for (int i = 0; i < sessions.Count; i++)
+            {
+                var session = sessions[i];
+                if ((int)session.GetProcessID == pid)
+                {
+                    session.SimpleAudioVolume.Volume = volume / 100f;
                 }
             }
         }
